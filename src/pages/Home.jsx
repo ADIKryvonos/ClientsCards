3377ac7219403bceea68558eb25bb04c122177b2
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-06-13 23:33:15 +0300

    add text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
 function Home() {
-  return <p>Tweets App</p>;
+  return (
+    <p
+      style={{
+        fontFamily: 'Montserrat',
+        fontWeight: '500',
+        fontSize: '20px',
+        lineHeight: '1.2',
+        color: '#373737',
+      }}
+    >
+      The Tweets-app is a testing app designed to allow users to follow or
+      unfollow other users. This type of functionality is commonly found in
+      social networking or content-sharing platforms where users can connect
+      with each other and receive updates on their activities.
+    </p>
+  );
 }
 
 export default Home;
